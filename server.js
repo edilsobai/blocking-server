1e42f21d6ae98b8d66e7890703a4805996c36f6b
Edil Usobaiev <usobaiev1@gmail.com>
2023-07-06 11:22:02 +0600

    Add pm2

--- server.js
+++ server.js
@@ -1,32 +1,23 @@
 const express = require("express");
-const cluster = require("cluster");
-const os = require("os") 
 const app = express();
 
 function delay(duration) {
-  const startTime = Date.now();
-  while (Date.now() - startTime < duration) {}
+  return new Promise(resolve => setTimeout(resolve, duration));
+ 
 }
 
 app.get("/", (req, res) => {
-  res.send("Home page");
+    res.send("Home page");
 });
 
-app.get("/timer", (req, res) => {
-  delay(9000);
-  res.send(`Process PID: ${process.pid}`);
+app.get("/timer", async (req, res) => {
+    await delay(4000);
+    res.send(`Beep! beeep! 
+    Process PID: ${process.pid}`);
 });
+
 
 console.log("Running server...");
-if (cluster.isMaster) {
-    console.log(`Master ${process.pid} is running`);
-    const NUM_WORKERS = os.cpus().length 
-    console.log(NUM_WORKERS)
-    for(let i = 0; i < NUM_WORKERS; i ++){
-    cluster.fork();
-    }
-  
-} else {
-  console.log(`Worker ${process.pid} is running`);
-  app.listen(5000, () => console.log("Running on port: 5000"));
-}
+console.log(`Master ${process.pid} is running`);
+console.log(`Worker ${process.pid} is running`);
+app.listen(5000, () => console.log("Running on port: 5000"));
